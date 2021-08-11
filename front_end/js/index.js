52d75d87fe231ad9f9d4757fad3b6215fd81df68
Fat2G <fatougassa@gmail.com>
2021-08-11 22:44:36 +0200

    creation fonction article generateur

--- front_end/js/index.js
+++ front_end/js/index.js
@@ -18,26 +18,58 @@
     for (let articles of data) {
       articlesContainer.innerHTML += `${articles.imageURL}<br> ${articles.name} ${articles.price}<br> <p> ${articles.description}</p><br>`;
     }
-  })
+  });
+ */
 
 
-  .then(data => console.log(data)); */
+/*
+  -T'ancrer sur un élément HTML
+  -faire de l'append de nouveaux elements HTML (article, image, etc)
+  -Toutes les informations se trouvent dans les données reçues
+  -Faire une boucle sur les données
+*/
 
+function article_gen(articlesContainer, image, productNamePrice, name, description, price){  
+  var articlesContainer = document.createElement("div");
+  articlesContainer.id =  "articlesContainer";
 
-/* <p>${articles.description}</p> */
+  var image = document.createElement("img");
+  image.id =  "image";
+  article.addChild(image);
 
-/*  for (let articles of data){
-      articlesContainer.innerHTML += `<div class= "card">
-      <img src= "${articles.imageUrl}"> <br>
-      <h1>${articles.name}</h1> <span>${articles.price}</span> <br>      
-      </div>`;
-    } */
+  var productNamePrice = document.createElement("div");
+  productNamePrice.id = "productNamePrice";
+  article.addChild(productNamePrice);
+  
+  var name = document.createElement("h3");
+  nom.id =  "name";
+  productNamePrice.addChild(name);
 
+  var prix = document.createElement("span");
+  price.id =  "price";
+  productNamePrice.addChild(price);
+  
+  var description = document.createElement("p");
+  description.id =  "description";
+  article.addChild(description); 
 
-  /*
-    -T'ancrer sur un élément HTML
-    -faire de l'append de nouveaux elements HTML (article, image, etc)
-    -Toutes les informations se trouvent dans les données reçues
-    -Faire une boucle sur les données
-  */
-
+  articlesContainer.addChild(article);
+}
+  
+function product(){
+  fetch('http://localhost:3000/api/cameras')
+    .then(response => response.json)
+    .then(data => console.log(data))
+    .then(data => {
+      const articleProduct = document.getElementsByClassName('articleProduct');
+      /* for (let articleProduct of data){
+        articleProduct.innerHTML += `
+        <img src="${articleProduct.imageUrl}"/>`
+      } */
+      for(let articleProduct of data){
+        articleProduct.innerHTML += article_gen()
+      }
+        
+    });
+}
+product();
