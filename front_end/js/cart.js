c6339a103e4bf6f507b33244fc019e5d8aa57859
Fat2G <fatougassa@gmail.com>
2021-09-16 02:41:36 +0200

    ajout bouton supprimer

--- front_end/js/cart.js
+++ front_end/js/cart.js
@@ -1,17 +1,13 @@
 /* récupération des données du localStorage */
 let addCart = JSON.parse(localStorage.getItem("camera"));
+console.log("cart");
 console.log(addCart);
 
 /* creation d'une constante relié à la div ayant l'id panier */
 const productCart = document.querySelector("#panier");
 
-/* const del = localStorage.removeItem('caméra');
-function deleteProduct(){
-  del;  
-} */
-
 /* si panier vide message qui s'affiche */
-if(addCart === null){
+if(addCart === null || addCart == 0){
   const cartEmpty = `
   <div class="cartEmpty">
     <h3> Le panier est vide </h3>
@@ -41,17 +37,42 @@ if(addCart === null){
       <div>
         <span> ${addCart[i].price} </span>
       </div>
-      <button type="button" onclick="/"> Effacer </button>
+      <button type="button" class="btnDelete"> Effacer </button>
     </div> 
     `;
   }
   productCart.innerHTML = cartStructure;
 }
+/* ------------------ BOUTON SUPPRIMER --------------------- */
+/* creation de la variable btnDelete */
+let btnDelete = document.querySelectorAll(".btnDelete");
+
+/* creation d'une boucle for pour supprimer la ligne choisie */
+for (let j = 0; j < btnDelete.length; j++) {
+  btnDelete[j].addEventListener("click", (event) => {
+    event.preventDefault();
+
+    /* selection de l'id que l'on veut supprimer */
+    let idDel = addCart[j].id;
+    console.log(idDel);
+
+    /* creation d'un nouveau tableau en utilisant la fonction filter */
+    addCart = addCart.filter( elementId => elementId.id !== idDel );
+
+    /* renvoie des données dans le localStorage */
+    localStorage.setItem("camera", JSON.stringify(addCart));
+
+    /* creation d'une alerte indiquant la suppression + rafraichissement de la page */
+    alert("Le produit a bien été supprimé");
+    window.location.reload();
+
+  })
+}
+
 
 /*--------------- VALIDATION DU FORMULAIRE --------------*/
 /* constante correspondant au bouton de confirmation de paiement */
 const btnPayCart = document.querySelector('#payBtn');
-console.log(btnPayCart);
 
 btnPayCart.addEventListener('click', (e) =>{
   /* on empeche le rechargement de la page et l'envoie du formulaire */
@@ -59,24 +80,45 @@ btnPayCart.addEventListener('click', (e) =>{
 
   /* données du formulaire et du panier envoyées dans le local storage */
   const formValues = {    
-    nom: document.querySelector('#nom').value,
-    prenom: document.querySelector('#prenom').value,
-    adresse: document.querySelector('#adresse').value,
-    ville: document.querySelector('#ville').value,
+    lastName: document.querySelector('#lastName').value,
+    firstName: document.querySelector('#firstName').value,
+    address: document.querySelector('#address').value,
+    city: document.querySelector('#city').value,
     email: document.querySelector('#email').value
   }
 
-  /* données du formulaire dans le localStorage */
+  /* données du formulaire dans le localStorage et stringify de l'objet formValues */
   localStorage.setItem('formulaire', JSON.stringify(localStorage.getItem('formValues')));
  
   /* données du formulaire et des produits placées dans un objet à envoyer vers le serveur */
+  // const cartFormToServer = {
+  //   addCart,
+  //   formValues 
+  // }
+
   const cartFormToServer = {
-    addCart,
-    formValues 
+    contact: {
+      lastName: document.querySelector('#lastName').value,
+      firstName: document.querySelector('#firstName').value,
+      address: document.querySelector('#address').value,
+      city: document.querySelector('#city').value,
+      email: document.querySelector('#email').value
+    },
+    products: addCart
   }
   console.log(cartFormToServer);
 
   /* envoie de l'objet réunissant le formulaire et les produits du panier vers le serveur */
+  const dataToServer = fetch('http://localhost:3000/api/cameras/order',{
+    method: 'POST',
+    /* conversion de l'objet 'cartFormToServer' en chaine de caracteres */
+    body: JSON.stringify('cartFormToServer'),
+    headers: {
+      'Content-type': 'application/json',
+    },
+  })
+  
+  
 
 
 
