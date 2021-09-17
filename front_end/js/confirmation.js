1f8e89a1220bf30ee596e34d0376b6df2b825c96
Fat2G <fatougassa@gmail.com>
2021-09-17 16:09:15 +0200

    page confirmation complete

---
+++ front_end/js/confirmation.js
@@ -0,0 +1,29 @@
+/* Récupération de l'id de la commance et du montant total dans localStorage */
+let orderId = localStorage.getItem("orderId");
+console.log(orderId);
+let totalPrice = localStorage.getItem("totalPrice");
+console.log(totalPrice);
+
+/* creation d'une constante relié à la div ayant l'id panier */
+const confirmation = document.querySelector("#bloc-confirmation");
+
+/* création du message de confirmation */
+const message = `
+  <h1 class="title">Récapitulatif de votre commande :</h1>
+  <p>La commande <span>${orderId}</span> d'un montant de <span>${totalPrice} €</span> a bien été effectuée.</p>
+  <p>Merci de votre achat.</p>
+  `
+
+/* Ajout des informations récupérées plus haut et ajouté au html */
+  confirmation.innerHTML = message;
+
+/* Suppression de toutes les clés du localStorage */
+localStorage.clear();
+
+/* Création bouton de retour et son évènement */
+const returnBtn = document.querySelector('#returnBtn');
+
+returnBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.location.href = 'index.html';
+});
