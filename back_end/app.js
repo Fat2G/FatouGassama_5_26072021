d5cb747e9b966b3bcef5d54d7783fbac64593c4f
Fat2G <fatougassa@gmail.com>
2021-09-17 16:51:32 +0200

    dossier back_end

---
+++ back_end/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+
+const cameraRoutes = require('./routes/camera');
+const teddyRoutes = require('./routes/teddy');
+const furnitureRoutes = require('./routes/furniture');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(express.static('images'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use('/api/cameras', cameraRoutes);
+app.use('/api/teddies', teddyRoutes);
+app.use('/api/furniture', furnitureRoutes);
+
+module.exports = app;
